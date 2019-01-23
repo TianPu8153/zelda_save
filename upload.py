babf0511999c3e1caeaf2ce8a779e4c4ea3b213c
tianpu8153 <2223169731@qq.com>
2019-01-23 09:14:53 +0800

    auto

--- upload.py
+++ upload.py
@@ -5,29 +5,31 @@ import time
 import psutil
 '''
 思路：
-1.先进入对应文件目录
-2.pull一边代码（如果没有更新可以跳过）
+1.先读取settings文件的设置
+2.pull一遍代码（如果没有必要可以跳过）
 3.打开exe文件并且记录pid
 4.while true循环查看pid是否存活time.sleep(60)
-5.pid不存活时，将需要的文件上传到git
+5.pid不存活时，如有需要，将目标文件夹下的文件上传到git
 '''
 #先读取设置
-f = open("settings.txt","r")   #设置文件对象
-data = f.readlines()  #直接将文件中按行读到list里，效果与方法2一样
-auto_pull=bool(data[0].split("=")[1].split("\n")[0])
-auto_push=bool(data[1].split("=")[1].split("\n")[0])
-f.close()
-#写pull的代码
+# f = open("settings.txt","r")   #设置文件
+# data = f.readlines()  #直接将文件中按行读到list里
+# auto_pull=bool(data[0].split("=")[1].split("\n")[0])
+# auto_push=bool(data[1].split("=")[1].split("\n")[0])
+# f.close()
+auto_pull=False
+auto_push=True
 if(auto_pull==True):
+	#将远程库的存档覆盖现存档
 	os.system("git fetch")
 	os.system("git diff origin/master")
 	os.system("git merge origin master")
-p = subprocess.Popen(r"C:\Program Files (x86)\Tencent\WeChat\WeChat.exe") 
-q = psutil.Process(p.pid) 
+p = subprocess.Popen(r"E:\Cemu+1.8.0+V0.25\cemu.exe") 
+q = psutil.Process(p.pid) #os.kill(pid,0)与subprocess.Popen配合有问题
 while(True):
-	time.sleep(2)
+	time.sleep(30)
 	try:
-		q.status()
+		q.status()#这里验证打开的程序是否已经关闭
 	except :
 		#pid对应的进程结束
 		#这里写文件上传的代码
@@ -36,7 +38,6 @@ while(True):
 			os.system(r'git commit -m "auto"')
 			os.system('git push origin master')
 		break
-input("输入任意键退出")
 
 
 
